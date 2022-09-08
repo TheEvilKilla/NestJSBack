41471eace8b0cfd51975da0f2e6f4d43f5d0c737
Isabela Ortiz <i.ortiz@uniandes.edu.co>
2022-09-08 18:34:47 -0500

    User and Pet Entity created

---
+++ src/user/user.entity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ name: string;
+ 
+ @Column()
+ login: string;
+ 
+ @Column()
+ password: string;
+ 
+ @Column()
+ city: string;
+
+ @Column()
+ image: string;
+}
