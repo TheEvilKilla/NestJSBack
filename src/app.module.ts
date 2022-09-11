2c3be98082857d7ab13fcfe929eb83eea562f671
TheEvilKilla <31011767+TheEvilKilla@users.noreply.github.com>
2022-09-11 11:36:46 -0500

    Revert "Andres"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuestionModule } from './question/question.module';
-import { ReviewModule } from './review/review.module';
-import { UserModule } from './user/user.module';
-import { PetModule } from './pet/pet.module';
+
 @Module({
-  imports: [QuestionModule, ReviewModule, UserModule, PetModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
